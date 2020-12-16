43efe4179f0fd2e6a1bd2c774888074369c9b371
Hayden Mottram <rc0043476@student.riversidecollege.ac.uk>
2020-12-16 10:21:39 +0000

    table layout panel

--- move picture/Form1.Designer.cs
+++ move picture/Form1.Designer.cs
@@ -33,14 +33,16 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(626, 314);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 66);
+            this.button1.Size = new System.Drawing.Size(94, 44);
             this.button1.TabIndex = 1;
             this.button1.Text = "move up";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,9 +60,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(626, 432);
+            this.button2.Location = new System.Drawing.Point(3, 53);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 71);
+            this.button2.Size = new System.Drawing.Size(94, 44);
             this.button2.TabIndex = 2;
             this.button2.Text = "move down";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,9 +70,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(501, 376);
+            this.button3.Location = new System.Drawing.Point(103, 53);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(119, 58);
+            this.button3.Size = new System.Drawing.Size(94, 44);
             this.button3.TabIndex = 3;
             this.button3.Text = "move left";
             this.button3.UseVisualStyleBackColor = true;
@@ -78,27 +80,42 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(757, 376);
+            this.button4.Location = new System.Drawing.Point(103, 3);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(120, 58);
+            this.button4.Size = new System.Drawing.Size(94, 44);
             this.button4.TabIndex = 4;
             this.button4.Text = "move right";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button4, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(668, 373);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 100);
+            this.tableLayoutPanel1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(942, 523);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -110,6 +127,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
